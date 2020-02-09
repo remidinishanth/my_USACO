b79cdb8199aafb1cf2659f24de4d291a048c46b4
Nishanth Reddy <remidinishanth@gmail.com>
2020-02-09 11:58:51 +0530

    Modular int functions from tourist
    https://codeforces.com/contest/1081/submission/47111844

---
+++ my_snippets/modular_int.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+const int md = 998244353;
+ 
+inline void add(int &a, int b) {
+  a += b;
+  if (a >= md) a -= md;
+}
+ 
+inline void sub(int &a, int b) {
+  a -= b;
+  if (a < 0) a += md;
+}
+ 
+inline int mul(int a, int b) {
+  return (int) ((long long) a * b % md);
+}
+ 
+inline int power(int a, long long b) {
+  int res = 1;
+  while (b > 0) {
+    if (b & 1) {
+      res = mul(res, a);
+    }
+    a = mul(a, a);
+    b >>= 1;
+  }
+  return res;
+}
+ 
+inline int inv(int a) {
+  a %= md;
+  if (a < 0) a += md;
+  int b = md, u = 0, v = 1;
+  while (a) {
+    int t = b / a;
+    b -= t * a; swap(a, b);
+    u -= t * v; swap(u, v);
+  }
+  assert(b == 1);
+  if (u < 0) u += md;
+  return u;
+}
