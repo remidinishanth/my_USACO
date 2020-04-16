e82ad7238f18e9403124bcc8168c785c181144f5
Nishanth Reddy <remidinishanth@gmail.com>
2020-04-17 02:08:14 +0530

    Update modular_int.cpp

--- my_snippets/modular_int.cpp
+++ my_snippets/modular_int.cpp
@@ -43,3 +43,27 @@ inline int inv(int a) {
   if (u < 0) u += md;
   return u;
 }
+
+#Um_nik from https://codeforces.com/contest/1336/submission/76831744
+const ll MOD = 998244353;
+ll add(ll x, ll y) {
+	x += y;
+	if (x >= MOD) return x - MOD;
+	return x;
+}
+ll sub(ll x, ll y) {
+	x -= y;
+	if (x < 0) return x + MOD;
+	return x;
+}
+ll mult(ll x, ll y) {
+	return (x * y) % MOD;
+}
+ll bin_pow(ll x, ll p) {
+	if (p == 0) return 1;
+	if (p & 1) return mult(x, bin_pow(x, p - 1));
+	return bin_pow(mult(x, x), p / 2);
+}
+ll rev(ll x) {
+	return bin_pow(x, MOD - 2);
+}
