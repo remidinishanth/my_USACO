4852ff4ab22731a8c71b0c658f885fc33fcd9d68
Nishanth Reddy <remidinishanth@gmail.com>
2020-05-08 23:23:51 +0530

    Update topological_sort.cpp

--- my_snippets/topological_sort.cpp
+++ my_snippets/topological_sort.cpp
@@ -41,3 +41,79 @@ for (int i = 0; i < V; i++) // this part is the same as finding CCs
 if (dfs_num[i] == UNVISITED)
 dfs2(i);
 reverse(ts.begin(), ts.end());
+
+// source tourist: https://codeforces.com/contest/1344/submission/79163314
+template <typename T>
+class graph {
+ public:
+  struct edge {
+    int from;
+    int to;
+    T cost;
+  };
+ 
+  vector<edge> edges;
+  vector<vector<int>> g;
+  int n;
+ 
+  graph(int _n) : n(_n) {
+    g.resize(n);
+  }
+ 
+  virtual int add(int from, int to, T cost) = 0;
+};
+ 
+template <typename T>
+class digraph : public graph<T> {
+ public:
+  using graph<T>::edges;
+  using graph<T>::g;
+  using graph<T>::n;
+ 
+  digraph(int _n) : graph<T>(_n) {
+  }
+ 
+  int add(int from, int to, T cost = 1) {
+    assert(0 <= from && from < n && 0 <= to && to < n);
+    int id = (int) edges.size();
+    g[from].push_back(id);
+    edges.push_back({from, to, cost});
+    return id;
+  }
+ 
+  digraph<T> reverse() const {
+    digraph<T> rev(n);
+    for (auto &e : edges) {
+      rev.add(e.to, e.from, e.cost);
+    }
+    return rev;
+  }
+};
+ 
+template <typename T>
+vector<int> find_topsort(const digraph<T> &g) {
+  vector<int> deg(g.n, 0);
+  for (int id = 0; id < (int) g.edges.size(); id++) {
+    deg[g.edges[id].to]++;
+  }
+  vector<int> x;
+  for (int i = 0; i < g.n; i++) {
+    if (deg[i] == 0) {
+      x.push_back(i);
+    }
+  }
+  for (int ptr = 0; ptr < (int) x.size(); ptr++) {
+    int i = x[ptr];
+    for (int id : g.g[i]) {
+      auto &e = g.edges[id];
+      int to = e.to;
+      if (--deg[to] == 0) {
+        x.push_back(to);
+      }
+    }
+  }
+  if ((int) x.size() != g.n) {
+    return vector<int>();
+  }
+  return x;
+}
