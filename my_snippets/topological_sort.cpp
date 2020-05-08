54ad542210f5b5d1dad24bc91505d7962b56ec11
Nishanth Reddy <remidinishanth@gmail.com>
2020-05-08 23:41:21 +0530

    Update topological_sort.cpp

--- my_snippets/topological_sort.cpp
+++ my_snippets/topological_sort.cpp
@@ -25,6 +25,23 @@ TopoSort<MX> T;
 F0R(i,m) { T.ae(j,k); }
 if (!T.sort(n)) { return -1; }
 
+// Source: ffao https://codeforces.com/contest/1344/submission/79215689
+
+vi topoSort(const vector<vi>& gr) {
+	vi indeg(sz(gr)), ret;
+	for (auto& li : gr) for (int x : li) indeg[x]++;
+	queue<int> q; // use priority queue for lexic. smallest ans.
+	rep(i,0,sz(gr)) if (indeg[i] == 0) q.push(-i);
+	while (!q.empty()) {
+		int i = -q.front(); // top() for priority queue
+		ret.push_back(i);
+		q.pop();
+		for (int x : gr[i])
+			if (--indeg[x] == 0) q.push(-x);
+	}
+	return ret;
+}
+
 // Source: CP3
 
 vi ts; // global vector to store the toposort in reverse order
