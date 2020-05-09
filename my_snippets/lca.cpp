83fb8b88a29bdf4b468fb229503bf1aa7d8eb1a9
Nishanth Reddy <remidinishanth@gmail.com>
2020-05-09 12:00:01 +0530

    Update lca.cpp

--- my_snippets/lca.cpp
+++ my_snippets/lca.cpp
@@ -65,3 +65,51 @@ memset(jump, 0, sizeof(jump));
 dfs(0, 1);
 
 // source: https://codeforces.com/contest/191/submission/1726763
+
+
+// source: monsoon https://codeforces.com/blog/entry/67138
+//Standard approach
+One of standard ways to implement constant-time LCA queries on a tree is to preprocess it by doing an Eulerian tour which creates an array of pairs (depth, index) for subsequent vertices visited on the tour, and then reduce an LCA query to RMQ query on a certain fragment of this array:
+
+const int N = 1<<LOGN;
+typedef pair<int,int> pii;
+int n, T;
+vector<int> adj[N];  // adjacency lists
+pii euler[2*N];  // Eulerian tour of pairs (depth, index)
+int tin[N];  // visit times for vertices
+
+void dfs(int v, int par, int depth) {
+  tin[v] = T;
+  euler[T++] = make_pair(depth, v);
+  FORE(i,adj[v]) if (*i != par) {
+    dfs(*i, v, depth+1);
+    euler[T++] = make_pair(depth, v);
+  }
+}
+
+dfs(0, -1, 0);
+
+int lca(int u, int v) {
+  return query_rmq(min(tin[u], tin[v]), max(tin[u], tin[v]) + 1).second;
+}
+The RMQ problem can be solved with a sparse table, which after 𝑂(𝑛log𝑛) preprocessing allows to answer RMQ queries in constant time:
+
+pii sparse[LOGN][N];
+int log[2*N];  // binary logarithm
+
+void init_rmq() {
+  REP(i,T) { sparse[0][i] = euler[i]; }
+  int logT = 0;
+  while ((1<<logT) < T) { ++logT; }
+  REP(f,logT) REP(i,T - (1<<f)) {
+    sparse[f+1][i] = min(sparse[f][i], sparse[f][i + (1<<f)]);
+  }
+  log[0] = -1;
+  FOR(i,1,T) { log[i] = 1 + log[i>>1]; }
+}
+
+pii query_rmq(int a, int b) {
+  // query range [a, b)
+  int f = log[b-a];
+  return min(sparse[f][a], sparse[f][b - (1<<f)]);
+}
