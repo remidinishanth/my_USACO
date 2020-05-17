68d5958dc7876e2109c89b5b1c365bbc0f168477
Nishanth Reddy <remidinishanth@gmail.com>
2020-05-17 11:01:12 +0530

    Update bimatching.cpp

--- my_snippets/bimatching.cpp
+++ my_snippets/bimatching.cpp
@@ -1,6 +1,6 @@
 vi match,vis;
 
-int Aug(int v){
+int Aug(int v){ // return 1 if an augmenting path is found
     if(vis[v]) return 0;
     vis[v]=1;
     for(int j=0;j<(int)Adj[v].size();j++){
@@ -16,6 +16,8 @@ int Aug(int v){
 // right set vertices are from [n+1..n+m]
 
 // in main
+// build unweighted bipartite graph with directed edge left->right set
+int MCBM=0;
 match.assign(V,-1);
 for(int v=0;v<n;v++){
     vis.assign(n,0);
@@ -26,3 +28,5 @@ for(int v=0;v<n;v++){
 // starting from free vertices on the left set
 
 // O(VE) as code runs V times and each DFS takes O(E) time
+
+// source: CP3 Augmenting Path Algorithm for Max Cardinality Bipartite Matching
