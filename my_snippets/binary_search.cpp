a3218d4095e6907d64e0d317c78954a5b5c38a7f
Nishanth Reddy <remidinishanth@gmail.com>
2020-05-17 12:46:59 +0530

    Update binary_search.cpp

--- my_snippets/binary_search.cpp
+++ my_snippets/binary_search.cpp
@@ -32,6 +32,56 @@ void lower_bound(){
 	return ans
 }
 
+// source: https://apps.topcoder.com/forums/?module=Thread&threadID=670168&start=0
+
+// Problem:
+// You want to find the smallest x in the given interval [a,b] such that some propety P(x) holds. 
+// You know that if P(x) and y>x, then also P(y). You also know that P(b) holds. 
+// This could be either an interval of integers, or of real numbers.
+
+// The most common application of binary search is the following. You have a non-decreasing function f, and a constant C. 
+// You have to find the smallest x for which f(x) = C (or, if there is none, the smallest x for which f(x) ≥ C). 
+// In this case, your property P(x) is "f(x) ≥ C".
+
+// Solution:
+
+If [a,b] is an interval of integers:
+int l = a, r = b;
+while(l < r) {
+  int m = (l+r) >> 1; // take the center of the interval
+  // replace the interval [a,b] with either [a,m] or [m+1,b],
+  // depending on whether P(m) is already satisfied
+  if(P(m)) r = m; else l = m+1;
+  }
+x=l;
+
+
+If [a,b] is an interval of real numbers:
+double l = a, r = b;
+for(int iterations=0; iterations<100; iterations++) {
+  double m = (a+b)/2;
+  if(P(m)) r = m; else l = m;
+  }
+x=l;
+
+
+// Discussion:
+
+// If you don't know that P(b) holds, use "b+1" for the upper bound. Note that P(x) is never checked for x which is 
+// the upper bound of the interval (since it is assumed to be true), thus, the binary search will either return the 
+// correct number, or "b+1", which means that there is no x in [a,b] for which P(x) is satisfied.
+
+// The most common and easiest use of this technique is looking for a certain value 'k' in a sorted array. 
+// In this case, if a[x] ≥ k and y>x, then also a[y] ≥ k, which means that we can use a[x] ≥ k as P(x). 
+// However, there are very many programming problems where such a P(x) property could be found.
+
+// Remember the code shown above well, especially the integer case. Make sure that your binary search works for 
+// intervals [a,a+1]—there are two possible answers for this problem, and after one step of binary search we should 
+// always have an interval [x,x] with a single possible answer. 
+// If you change one of the three places "m=(l+r)>>1", "r=m", "l=m+1", even by adding or subtracting one to the right 
+// side of one of the assignments, the result will probably fail (one of two subintervals is likely to be either 
+// nonsense [a,a-1], which means that there is an error, or [a,a+1] again, which means that the algorithm loops infinitely).
+
 
 // Problem: Unimodal functions
 // You have a function f in an interval [a,b] of real numbers, and you want to find the x for which 
