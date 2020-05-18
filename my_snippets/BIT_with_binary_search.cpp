77dc800dacb197515aa63c52b2a3f831696a685e
Nishanth Reddy <remidinishanth@gmail.com>
2020-05-18 08:53:52 +0530

    Update BIT_with_binary_search.cpp

--- my_snippets/BIT_with_binary_search.cpp
+++ my_snippets/BIT_with_binary_search.cpp
@@ -62,3 +62,117 @@ Idea: Let idx be an index of BIT. Let r be the position in idx of its last non-z
 binary notation, i.e., r is the position of the least significant non-zero bit of idx. 
 bit[idx] holds the sum of frequencies for indices (idx - 2^r + 1) through idx, inclusive
 */
+
+// source: neal https://codeforces.com/contest/1354/submission/80534648
+
+
+template<typename T>
+struct fenwick_tree {
+    int tree_n = 0;
+    T tree_sum = 0;
+    vector<T> tree;
+ 
+    fenwick_tree(int n = -1) {
+        if (n >= 0)
+            init(n);
+    }
+ 
+    void init(int n) {
+        tree_n = n;
+        tree_sum = 0;
+        tree.assign(tree_n + 1, 0);
+    }
+ 
+    // O(n) initialization of the Fenwick tree.
+    template<typename T_array>
+    void build(const T_array &initial) {
+        assert(int(initial.size()) == tree_n);
+        tree_sum = 0;
+ 
+        for (int i = 1; i <= tree_n; i++) {
+            tree[i] = initial[i - 1];
+            tree_sum += initial[i - 1];
+ 
+            for (int k = (i & -i) >> 1; k > 0; k >>= 1)
+                tree[i] += tree[i - k];
+        }
+    }
+ 
+    // index is in [0, tree_n).
+    void update(int index, const T &change) {
+        assert(0 <= index && index < tree_n);
+        tree_sum += change;
+ 
+        for (int i = index + 1; i <= tree_n; i += i & -i)
+            tree[i] += change;
+    }
+ 
+    // Returns the sum of the range [0, count).
+    T query(int count) const {
+        assert(count <= tree_n);
+        T sum = 0;
+ 
+        for (int i = count; i > 0; i -= i & -i)
+            sum += tree[i];
+ 
+        return sum;
+    }
+ 
+    // Returns the sum of the range [start, tree_n).
+    T query_suffix(int start) const {
+        return tree_sum - query(start);
+    }
+ 
+    // Returns the sum of the range [a, b).
+    T query(int a, int b) const {
+        return query(b) - query(a);
+    }
+ 
+    // Returns the element at index a in O(1) amortized across every index. Equivalent to query(a, a + 1).
+    T get(int a) const {
+        assert(0 <= a && a < tree_n);
+        int above = a + 1;
+        T sum = tree[above];
+        above -= above & -above;
+ 
+        while (a != above) {
+            sum -= tree[a];
+            a -= a & -a;
+        }
+ 
+        return sum;
+    }
+ 
+    bool set(int index, T value) {
+        assert(0 <= index && index < tree_n);
+        T current = get(index);
+ 
+        if (current == value)
+            return false;
+ 
+        update(index, value - current);
+        return true;
+    }
+ 
+    // Returns the largest p in `[0, tree_n]` such that `query(p) <= sum`. Returns -1 if no such p exists (`sum < 0`).
+    // Can be used as an ordered set/multiset on indices in `[0, tree_n)` by using the tree as a 0/1 or frequency array:
+    // `set(index, 1)` is equivalent to insert(index). `update(index, +1)` is equivalent to multiset.insert(index).
+    // `set(index, 0)` or `update(index, -1)` are equivalent to erase(index).
+    // `get(index)` provides whether index is present or not, or the count of index (if multiset).
+    // `query(index)` provides the count of elements < index.
+    // `find_last_prefix(k)` finds the k-th smallest element (0-indexed). Returns `tree_n` for `sum >= set.size()`.
+    int find_last_prefix(T sum) const {
+        if (sum < 0)
+            return -1;
+ 
+        int prefix = 0;
+ 
+        for (int k = 31 - __builtin_clz(tree_n); k >= 0; k--)
+            if (prefix + (1 << k) <= tree_n && tree[prefix + (1 << k)] <= sum) {
+                prefix += 1 << k;
+                sum -= tree[prefix];
+            }
+ 
+        return prefix;
+    }
+};
