2a7fbc6983eb30af45acfe77c125470d432b54ea
Nishanth Reddy <remidinishanth@gmail.com>
2020-05-18 14:36:53 +0530

    Update pbds.cpp

--- my_snippets/pbds.cpp
+++ my_snippets/pbds.cpp
@@ -58,23 +58,23 @@ typedef tree<int,
 // the second — the number of items in a set that are strictly smaller than our item. Example of use:
 
 
-    ordered_set X;
-    X.insert(1);
-    X.insert(2);
-    X.insert(4);
-    X.insert(8);
-    X.insert(16);
+ordered_set X;
+X.insert(1);
+X.insert(2);
+X.insert(4);
+X.insert(8);
+X.insert(16);
 
-    cout<<*X.find_by_order(1)<<endl; // 2
-    cout<<*X.find_by_order(2)<<endl; // 4
-    cout<<*X.find_by_order(4)<<endl; // 16
-    cout<<(end(X)==X.find_by_order(6))<<endl; // true
+cout << *X.find_by_order(1) << endl;             // 2
+cout << *X.find_by_order(2) << endl;             // 4
+cout << *X.find_by_order(4) << endl;             // 16
+cout << (end(X) == X.find_by_order(6)) << endl;  // true
 
-    cout<<X.order_of_key(-5)<<endl;  // 0
-    cout<<X.order_of_key(1)<<endl;   // 0
-    cout<<X.order_of_key(3)<<endl;   // 2
-    cout<<X.order_of_key(4)<<endl;   // 2
-    cout<<X.order_of_key(400)<<endl; // 5
+cout << X.order_of_key(-5) << endl;   // 0
+cout << X.order_of_key(1) << endl;    // 0
+cout << X.order_of_key(3) << endl;    // 2
+cout << X.order_of_key(4) << endl;    // 2
+cout << X.order_of_key(400) << endl;  // 5
 
 // Usage: 
 
