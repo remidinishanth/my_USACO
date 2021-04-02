5459e6a7ff4c548b0b660d6760675b085b9d0cd7
N̷̩̩͇̺̲͍̗̟̋̔ĩ̴̡̃̀͋͌̾̄̑͒͝ș̸̢̡͉̥̆̓̾̑̊͂͜h̶̡̳̐̓̀̏̒̉̐͘̚͜ͅą̴̞̻͕̠͕̱̦̰̿ͅǹ̴̞͔͔̼͇ṯ̶̼̝̞͕̝̼̫̭̿ḩ̸̧̟̪̪̞̈́̎̏̈͋͠ ̵̛͉͓͎̞͆̆͌̕͝Ŕ̶̭̩̤̪̗͕͓̐̒e̷̮̫͈̹̮̓̀̍̈́̇́̐͑͝͝ͅd̸̬̎̈́̌̔d̴͙͓̅̔̅͐͌̇̅͒̒ý̷̢̤͉̞̜̰͎̈́́̀͐̈́̕͝ <remidinishanth@gmail.com>
2021-04-02 22:20:18 +0530

    Update modular_int.cpp

--- my_snippets/modular_int.cpp
+++ my_snippets/modular_int.cpp
@@ -67,3 +67,15 @@ ll bin_pow(ll x, ll p) {
 ll rev(ll x) {
 	return bin_pow(x, MOD - 2);
 }
+
+// source: https://codeforces.com/contest/1264/submission/66344993 (ksun48)
+using ll = long long;
+ll modinv(ll a, ll m) {
+	assert(m > 0);
+	if (m == 1) return 0;
+	a %= m;
+	if (a < 0) a += m;
+	assert(a != 0);
+	if (a == 1) return 1;
+	return m - modinv(m, a) * m / a;
+}
