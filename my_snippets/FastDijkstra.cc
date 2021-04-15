961cde0bb3e3c2393bd56d2ba98fe9c321afdd77
N̷̩̩͇̺̲͍̗̟̋̔ĩ̴̡̃̀͋͌̾̄̑͒͝ș̸̢̡͉̥̆̓̾̑̊͂͜h̶̡̳̐̓̀̏̒̉̐͘̚͜ͅą̴̞̻͕̠͕̱̦̰̿ͅǹ̴̞͔͔̼͇ṯ̶̼̝̞͕̝̼̫̭̿ḩ̸̧̟̪̪̞̈́̎̏̈͋͠ ̵̛͉͓͎̞͆̆͌̕͝Ŕ̶̭̩̤̪̗͕͓̐̒e̷̮̫͈̹̮̓̀̍̈́̇́̐͑͝͝ͅd̸̬̎̈́̌̔d̴͙͓̅̔̅͐͌̇̅͒̒ý̷̢̤͉̞̜̰͎̈́́̀͐̈́̕͝ <remidinishanth@gmail.com>
2021-04-16 00:19:19 +0530

    Update FastDijkstra.cc

--- my_snippets/FastDijkstra.cc
+++ my_snippets/FastDijkstra.cc
@@ -72,3 +72,154 @@ In a directed graph, if we apply Dijkstra’s algorithm from vertex i, we can ob
 	
 Also, if we reverse all the directions of edges and similarly apply Dijkstra’s algorithm from vertex i, we can find the minimum distance from each 
 vertex j to vertex i in the original graph.
+
+	
+
+// source: https://github.com/ta7uw/atcoder-cpp/blob/master/lib/graph/dijkstra.cpp	
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using P = pair<ll, ll>;
+using Graph = vector<vector<ll>>;
+#define rep(i, n) for(ll i=0;i<(ll)(n);i++)
+#define rep2(i, m, n) for(ll i=m;i<(ll)(n);i++)
+#define rrep(i, n, m) for(ll i=n;i>=(ll)(m);i--)
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+const int ddx[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+const int ddy[8] = {1, 1, 0, -1, -1, -1, 0, 1};
+const ll MOD = 1000000007;
+const ll INF = 1000000000000000000L;
+
+/**
+ * Library
+ * --------------------------------------------------------
+ */
+struct edge {
+    int from, to;
+    ll cost;
+};
+
+class Dijkstra {
+    int V;
+    vector<vector<edge>> edges;
+    vector<ll> dist;
+    vector<ll> prev;
+    vector<ll> route_count;
+
+public:
+    Dijkstra(int v) : V(v) {
+        edges.resize(V);
+        dist.resize(V, INF);
+        prev.resize(V, INF);
+        route_count.resize(V, 0);
+    }
+
+    void add_edge(int from, int to, ll cost) {
+        edges[from].push_back({from, to, cost});
+    }
+
+    /**
+     * @param goal 0-indexed
+     * @return return cost to reach the goal. return INF if the goal is unreachable.
+     */
+    ll get_cost(int goal) {
+        return dist[goal];
+    }
+
+    /**
+     * @param goal 0-indexed
+     * @return return total route count to reach the goal ( result value % MOD )
+     */
+    ll get_route_count(int goal) {
+        return route_count[goal];
+    }
+
+    /**
+     * @param goal 0-indexed
+     * @return return route from start to goal
+     */
+    vector<ll> get_route(int goal) {
+        vector<ll> res;
+        res.push_back(goal);
+        ll dest = goal;
+        while (prev[dest] != INF) {
+            res.push_back(prev[dest]);
+            dest = prev[dest];
+        }
+        reverse(res.begin(), res.end());
+        return res;
+    }
+
+    void exec(int start) {
+        dist[start] = 0;
+        route_count[start] = 1;
+        priority_queue<P, vector<P>, greater<>> queue;
+        queue.push({dist[start], start});
+        while (!queue.empty()) {
+            P p = queue.top();
+            queue.pop();
+            ll prev_cost = p.first;
+            ll src = p.second;
+            if (dist[src] < prev_cost) continue;
+
+            for (edge &e : edges[src]) {
+                ll to = e.to;
+                ll cost = e.cost;
+                if (cost != INF && dist[to] > dist[src] + cost) {
+                    dist[to] = dist[src] + cost;
+                    queue.push({dist[to], to});
+                    prev[to] = src;
+                    route_count[to] = route_count[src];
+                } else if (dist[to] == dist[src] + cost) {
+                    route_count[to] += route_count[src];
+                    route_count[to] %= MOD;
+                }
+            }
+        }
+    }
+};
+/**
+ * --------------------------------------------------------
+ */
+
+/**
+ * ABC 035 D - トレジャーハント
+ * https://atcoder.jp/contests/abc035/submissions/7668866
+ */
+void Main() {
+    int N, M, T;
+    cin >> N >> M >> T;
+    vector<ll> t_list(N);
+    rep(i, N) cin >> t_list[i];
+    Dijkstra dijkstra1(N);
+    Dijkstra dijkstra2(N);
+    rep(i, M) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        a--;
+        b--;
+        dijkstra1.add_edge(a, b, c);
+        dijkstra2.add_edge(b, a, c);
+    }
+
+    dijkstra1.exec(0);
+    dijkstra2.exec(0);
+    ll ans = t_list[0] * T;
+    rep2(i, 1, N) {
+        ll go = dijkstra1.get_cost(i);
+        ll back = dijkstra2.get_cost(i);
+        if (go != INF && back != INF) ans = max(ans, t_list[i] * (T - go - back));
+    }
+    cout << ans << '\n';
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cout << fixed << setprecision(15);
+    Main();
+    return 0;
+}
