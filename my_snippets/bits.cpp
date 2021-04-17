faaaa1ebf4b8224a7a4c4002515127d34ce64eaf
N̷̩̩͇̺̲͍̗̟̋̔ĩ̴̡̃̀͋͌̾̄̑͒͝ș̸̢̡͉̥̆̓̾̑̊͂͜h̶̡̳̐̓̀̏̒̉̐͘̚͜ͅą̴̞̻͕̠͕̱̦̰̿ͅǹ̴̞͔͔̼͇ṯ̶̼̝̞͕̝̼̫̭̿ḩ̸̧̟̪̪̞̈́̎̏̈͋͠ ̵̛͉͓͎̞͆̆͌̕͝Ŕ̶̭̩̤̪̗͕͓̐̒e̷̮̫͈̹̮̓̀̍̈́̇́̐͑͝͝ͅd̸̬̎̈́̌̔d̴͙͓̅̔̅͐͌̇̅͒̒ý̷̢̤͉̞̜̰͎̈́́̀͐̈́̕͝ <remidinishanth@gmail.com>
2021-04-17 15:08:01 +0530

    Update bits.cpp

--- my_snippets/bits.cpp
+++ my_snippets/bits.cpp
@@ -203,3 +203,30 @@ bool isPowerOfTwo(int x)
     // x will check if x == 0 and !(x & (x - 1)) will check if x is a power of 2 or not
     return (x && !(x & (x - 1)));
 }
+
+
+Tricks with Bits:
+
+1) x ^ ( x & (x-1)) : Returns the rightmost 1 in binary representation of x.
+
+As explained above, (x & (x - 1)) will have all the bits equal to the x except for the rightmost 1 in x. So if we do bitwise XOR of x and (x & (x-1)), it will simply return the rightmost 1. Let’s see an example.
+x = 10 = (1010)2
+x & (x-1) = (1010)2 & (1001)2 = (1000)2
+x ^ (x & (x-1)) = (1010)2 ^ (1000)2 = (0010)2
+
+2) x & (-x) : Returns the rightmost 1 in binary representation of x
+
+(-x) is the two’s complement of x. (-x) will be equal to one’s complement of x plus 1.
+Therefore (-x) will have all the bits flipped that are on the left of the rightmost 1 in x. So x & (-x) will return rightmost 1.
+
+x = 10 = (1010)2
+(-x) = -10 = (0110)2
+x & (-x) = (1010)2 & (0110)2 = (0010)2
+
+3) x | (1 << n) : Returns the number x with the nth bit set.
+
+(1 << n) will return a number with only nth bit set. So if we OR it with x it will set the nth bit of x.
+x = 10 = (1010)2 n = 2
+1 << n = (0100)2
+x | (1 << n) = (1010)2 | (0100)2 = (1110)2
+
