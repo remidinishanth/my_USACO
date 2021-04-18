a1d8cf767c37d3859bf9a4c2a631d6bdfb170514
N̷̩̩͇̺̲͍̗̟̋̔ĩ̴̡̃̀͋͌̾̄̑͒͝ș̸̢̡͉̥̆̓̾̑̊͂͜h̶̡̳̐̓̀̏̒̉̐͘̚͜ͅą̴̞̻͕̠͕̱̦̰̿ͅǹ̴̞͔͔̼͇ṯ̶̼̝̞͕̝̼̫̭̿ḩ̸̧̟̪̪̞̈́̎̏̈͋͠ ̵̛͉͓͎̞͆̆͌̕͝Ŕ̶̭̩̤̪̗͕͓̐̒e̷̮̫͈̹̮̓̀̍̈́̇́̐͑͝͝ͅd̸̬̎̈́̌̔d̴͙͓̅̔̅͐͌̇̅͒̒ý̷̢̤͉̞̜̰͎̈́́̀͐̈́̕͝ <remidinishanth@gmail.com>
2021-04-18 17:04:09 +0530

    Update modular_int.cpp

--- my_snippets/modular_int.cpp
+++ my_snippets/modular_int.cpp
@@ -68,6 +68,72 @@ ll rev(ll x) {
 	return bin_pow(x, MOD - 2);
 }
 
+const ll MOD = (ll)1e9 + 7;
+ll add(ll x, ll y) {
+	x += y;
+	if (x >= MOD) return x - MOD;
+	return x;
+}
+ll sub(ll x, ll y) {
+	x -= y;
+	if (x < 0) return x + MOD;
+	return x;
+}
+ll mult(ll x, ll y) {
+	return (x * y) % MOD;
+}
+ll bin_pow(ll x, ll p) {
+	if (p == 0) return 1;
+	if (p & 1) return mult(x, bin_pow(x, p - 1));
+	return bin_pow(mult(x, x), p / 2);
+}
+ll rev(ll x) {
+	return bin_pow(x, MOD - 2);
+}
+ 
+const int N = 200200;
+ll f[N], rf[N];
+ll pA[N], pB[N];
+int n;
+ll A, B, C;
+ 
+ll getC(int n, int k) {
+	if (k < 0 || k > n) return 0;
+	return mult(f[n], mult(rf[k], rf[n - k]));
+}
+ 
+int main()
+{
+//	freopen("input.txt", "r", stdin);
+//	freopen("output.txt", "w", stdout);
+ 
+	f[0] = 1;
+	for (int i = 1; i < N; i++)
+		f[i] = mult(f[i - 1], i);
+	rf[N - 1] = rev(f[N - 1]);
+	for (int i = N - 1; i > 0; i--)
+		rf[i - 1] = mult(rf[i], i);
+ 
+	scanf("%d%lld%lld%lld", &n, &A, &B, &C);
+	pA[0] = pB[0] = 1;
+	pA[1] = mult(A, rev(A + B));
+	pB[1] = mult(B, rev(A + B));
+	for (int i = 2; i < N; i++) {
+		pA[i] = mult(pA[i - 1], pA[1]);
+		pB[i] = mult(pB[i - 1], pB[1]);
+	}
+	ll ans = 0;
+	for (int k = 0; k < n; k++) {
+		ll cur = getC(n - 1 + k, k);
+		ans = add(ans, mult(mult(cur, n + k), add(mult(pA[n], pB[k]), mult(pB[n], pA[k]))));
+	}
+	ans = mult(ans, mult(100, rev(A + B)));
+	printf("%lld\n", ans);
+ 
+	return 0;
+}
+// source: https://atcoder.jp/contests/m-solutions2019/submissions/5729508
+
 // source: https://codeforces.com/contest/1264/submission/66344993 (ksun48)
 using ll = long long;
 ll modinv(ll a, ll m) {
