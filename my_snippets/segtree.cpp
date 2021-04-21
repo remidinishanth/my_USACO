cf02a75bb318421190e8c15677187b4437c9486f
N̷̩̩͇̺̲͍̗̟̋̔ĩ̴̡̃̀͋͌̾̄̑͒͝ș̸̢̡͉̥̆̓̾̑̊͂͜h̶̡̳̐̓̀̏̒̉̐͘̚͜ͅą̴̞̻͕̠͕̱̦̰̿ͅǹ̴̞͔͔̼͇ṯ̶̼̝̞͕̝̼̫̭̿ḩ̸̧̟̪̪̞̈́̎̏̈͋͠ ̵̛͉͓͎̞͆̆͌̕͝Ŕ̶̭̩̤̪̗͕͓̐̒e̷̮̫͈̹̮̓̀̍̈́̇́̐͑͝͝ͅd̸̬̎̈́̌̔d̴͙͓̅̔̅͐͌̇̅͒̒ý̷̢̤͉̞̜̰͎̈́́̀͐̈́̕͝ <remidinishanth@gmail.com>
2021-04-21 21:40:45 +0530

    Update segtree.cpp

--- my_snippets/segtree.cpp
+++ my_snippets/segtree.cpp
@@ -316,3 +316,83 @@ ll sum(int v, int tl, int tr, int l, int r) {
 }
 
 // Tested with SPOJ HORRIBLE - Horrible Queries
+
+
+
+// kmjp
+
+Static ll const def = - 1LL << 50 ;
+ template < class V, int NV> class SegTree_3 {
+ public :
+	vector <V> val, ma;
+	SegTree_3 () {
+		int i;
+		val.resize (NV * 2 , 0 ); ma.resize (NV * 2 , 0 );
+		FOR (i, NV) val [i + NV] = ma [i + NV] = def;
+		for (i = NV- 1 ; i> = 1 ; i--) ma [i] = max (ma [ 2 * i], ma [ 2 * i + 1 ]);
+	};
+	
+	V getval ( int x, int y, int l = 0 , int r = NV, int k = 1 ) {
+		 if (r <= x || y <= l) return def;
+		 if (x <= l && r < = y) return ma [k];
+		 return val [k] + max (getval (x, y, l, (l + r) / 2 , k * 2 ), getval (x, y, (l + r) / 2 , r, k * 2 + 1 )));
+	}
+	
+	void update ( int x, int y, V v, int l = 0 , int r = NV, int k = 1 ) {
+		 if (l> = r) return ;
+		 if (x <= l && r <= y) {
+			val [k] + = v;
+			ma [k] + = v;
+		}
+		else  if (l <y && x <r) {
+			update (x, y, v, l, (l + r) / 2 , k * 2 );
+			update (x, y, v, (l + r) / 2 , r, k * 2 + 1 );
+			ma [k] = val [k] + max (ma [k * 2 ], ma [k * 2 + 1 ]);
+		}
+	}
+};
+
+template < class V, int NV> class SegTree_2 {
+ public :
+	vector <V> val;
+	static V constant def = 0 ;
+	V comp (V l, V r) { return __gcd (l, r);};
+	
+	SegTree_2 () {val = vector <V> (NV * 2 , def);};
+	V getval ( int x, int y, int l = 0 , int r = NV, int k = 1 ) { // x <= i <y 
+		if (r <= x || y <= l) return def;
+		 if (x <= l && r <= y) return val [k];
+		 return comp (getval (x, y, l, (l + r) / 2 , k * 2 ), getval (x, y, (l +) r) / 2 , r, k * 2 + 1 )));
+	}
+	void update ( int entry, V v) {
+		entry + = NV;
+		val [entry] = v;
+		while (entry> 1 ) entry >> = 1 , val [entry] = comp (val [entry * 2 ], val [entry * 2 + 1 ]);
+	}
+};
+
+SegTree_3 <ll, 1 << 17 > stmax;
+SegTree_2 < int , 1 << 17 > stgcd;
+
+template<class V,int NV> class SegTree_1 {
+public:
+	vector<V> val;
+	 static V const def=1LL<<30;
+	V comp(V l,V r){ return min(l,r);};
+	
+	SegTree_1(){val=vector<V>(NV*2,def);};
+	V getval(int x,int y,int l=0,int r=NV,int k=1) { // x<=i<y
+		if(r<=x || y<=l) return def;
+		if(x<=l && r<=y) return val[k];
+		return comp(getval(x,y,l,(l+r)/2,k*2),getval(x,y,(l+r)/2,r,k*2+1));
+	}
+	void update(int entry, V v) {
+		entry += NV;
+		val[entry]=comp(v,val[entry]); //上書きかチェック
+		while(entry>1) entry>>=1, val[entry]=comp(val[entry*2],val[entry*2+1]);
+	}
+};
+SegTree_1<int,1<<18> st;
+ 
+// source: https://codeforces.com/contest/1516/submission/113760111
+// source: https://kmjp.hatenablog.jp/entry/2018/01/03/0930
