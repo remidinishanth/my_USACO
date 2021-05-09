13ea861144392746d1a170387313854ce5b5a05f
N̷̩̩͇̺̲͍̗̟̋̔ĩ̴̡̃̀͋͌̾̄̑͒͝ș̸̢̡͉̥̆̓̾̑̊͂͜h̶̡̳̐̓̀̏̒̉̐͘̚͜ͅą̴̞̻͕̠͕̱̦̰̿ͅǹ̴̞͔͔̼͇ṯ̶̼̝̞͕̝̼̫̭̿ḩ̸̧̟̪̪̞̈́̎̏̈͋͠ ̵̛͉͓͎̞͆̆͌̕͝Ŕ̶̭̩̤̪̗͕͓̐̒e̷̮̫͈̹̮̓̀̍̈́̇́̐͑͝͝ͅd̸̬̎̈́̌̔d̴͙͓̅̔̅͐͌̇̅͒̒ý̷̢̤͉̞̜̰͎̈́́̀͐̈́̕͝ <remidinishanth@gmail.com>
2021-05-09 23:37:18 +0530

    Update disjoint_set_union_DSU.cpp

--- my_snippets/disjoint_set_union_DSU.cpp
+++ my_snippets/disjoint_set_union_DSU.cpp
@@ -67,3 +67,33 @@ struct UnionFind{
   }
 };
 // source: User:beet https://atcoder.jp/contests/zone2021/submissions/22207964
+
+	   
+// Benq
+struct DSU {
+	vector<int> e; void init(int N) { e = vector<int>(N,-1); }
+	int get(int x) { return e[x] < 0 ? x : e[x] = get(e[x]); }
+	bool sameSet(int a, int b) { return get(a) == get(b); }
+	int size(int x) { return -e[get(x)]; }
+	bool unite(int x, int y) { // union by size
+		x = get(x), y = get(y); if (x == y) return 0;
+		if (e[x] > e[y]) swap(x,y);
+		e[x] += e[y]; e[y] = x; return 1;
+	}
+};
+	   
+// Usage
+DSU D;
+int n,m;
+vector<int> comps;
+
+void init() {
+	cin >> n >> m; D.init(n);
+	for (int i = 0; i < m; ++i) {
+		int a,b; cin >> a >> b;
+		D.unite(a-1,b-1);
+	}
+	for (int i = 0; i < n; ++i) if (D.get(i) == i)
+		comps.push_back(D.size(i));
+}
+// source: https://usaco.guide/plat/bitsets?lang=cpp
