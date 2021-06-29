2d173fe24f50934f8b2e5fa62c96e25759b980ea
N̷̩̩͇̺̲͍̗̟̋̔ĩ̴̡̃̀͋͌̾̄̑͒͝ș̸̢̡͉̥̆̓̾̑̊͂͜h̶̡̳̐̓̀̏̒̉̐͘̚͜ͅą̴̞̻͕̠͕̱̦̰̿ͅǹ̴̞͔͔̼͇ṯ̶̼̝̞͕̝̼̫̭̿ḩ̸̧̟̪̪̞̈́̎̏̈͋͠ ̵̛͉͓͎̞͆̆͌̕͝Ŕ̶̭̩̤̪̗͕͓̐̒e̷̮̫͈̹̮̓̀̍̈́̇́̐͑͝͝ͅd̸̬̎̈́̌̔d̴͙͓̅̔̅͐͌̇̅͒̒ý̷̢̤͉̞̜̰͎̈́́̀͐̈́̕͝ <remidinishanth@gmail.com>
2021-06-29 19:36:24 +0530

    Update ref.kt

--- ref.kt
+++ ref.kt
@@ -375,3 +375,21 @@ data class Person(val name: String) {
     var age: Int = 0
 }
 val person1 = Person("John"); person1.age = 10
+
+
+// Scanning Input
+
+private fun readLn() = readLine()!! // string line
+private fun readInt() = readLn().toInt() // single int
+private fun readLong() = readLn().toLong() // single long
+private fun readDouble() = readLn().toDouble() // single double
+private fun readStrings() = readLn().split(" ") // list of strings
+private fun readInts() = readStrings().map { it.toInt() } // list of ints
+private fun readLongs() = readStrings().map { it.toLong() } // list of longs
+private fun readDoubles() = readStrings().map { it.toDouble() } // list of doubles
+ 
+fun main(args: Array<String>) {
+    var t = readInt()
+    for (i in 0 until t) {
+        var (a, b) = readInts()
+        println(a + b)
