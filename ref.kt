717a3bc987d8f2e2c0e4e3aac432b8ac4171d2e6
N̷̩̩͇̺̲͍̗̟̋̔ĩ̴̡̃̀͋͌̾̄̑͒͝ș̸̢̡͉̥̆̓̾̑̊͂͜h̶̡̳̐̓̀̏̒̉̐͘̚͜ͅą̴̞̻͕̠͕̱̦̰̿ͅǹ̴̞͔͔̼͇ṯ̶̼̝̞͕̝̼̫̭̿ḩ̸̧̟̪̪̞̈́̎̏̈͋͠ ̵̛͉͓͎̞͆̆͌̕͝Ŕ̶̭̩̤̪̗͕͓̐̒e̷̮̫͈̹̮̓̀̍̈́̇́̐͑͝͝ͅd̸̬̎̈́̌̔d̴͙͓̅̔̅͐͌̇̅͒̒ý̷̢̤͉̞̜̰͎̈́́̀͐̈́̕͝ <remidinishanth@gmail.com>
2021-10-07 10:42:14 +0530

    Update ref.kt

--- ref.kt
+++ ref.kt
@@ -140,7 +140,7 @@ for (i in 4 downTo 1 step 2) print(i) // 42
 for (i in 1..100) { ... }  // closed range: includes 100
 for (i in 1 until 100) { ... } // half-open range: does not include 100
 for (x in 2..10 step 2) { ... }
-for (x in 10 downTo 1) { ... }
+for (x in 10 downTo 1) { ... } // 1 is included
 if (x in 1..10) { ... }
 
 var language = arrayOf("Ruby", "Koltin", "Python" "Java")
